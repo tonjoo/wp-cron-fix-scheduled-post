0a2fb9b09dda313f90522bc7f25f53d10c8b770f
Todi Adiyatmo <todiadiyatmo@tonjoo.com>
2016-08-06 11:49:33 +0700

    fix cron

--- wp-cron-fix-scheduled-post.php
+++ wp-cron-fix-scheduled-post.php
@@ -27,27 +27,23 @@ function wp_cron_fix_scheduled_post_deactive() {
 	wp_clear_scheduled_hook('wp_cron_fix_scheduled_post');
 }
 
+add_action('wp_cron_fix_scheduled_post', 'wp_cron_fix_scheduled_post_do');
 
-
-add_action('init','wp_cron_fix_scheduled_post_init');;
-
-function wp_cron_fix_scheduled_post_init() {
-	
-	add_action('wp_cron_fix_scheduled_post', 'wp_cron_fix_scheduled_post_do');
-	
-}
-
-function wp_cron_fix_scheduled_post_do() 
-{
+function wp_cron_fix_scheduled_post_do( $from_front = false )  {
 	/*
 	 * Bail if needed
 	 */
 	$wp_cron_fix_scheduled_missed = get_transient( 'wp_cron_fix_scheduled_missed_timeout' );
 
-	if ( ( $wp_cron_fix_scheduled_missed !== false ) && ( $wp_cron_fix_scheduled_missed > ( time() - ( 200 ) ) ) )
-		return;
+	if ( ( $wp_cron_fix_scheduled_missed !== false ) && ( $wp_cron_fix_scheduled_missed > ( time() - ( 100 ) ) ) ) {
 
-	set_transient( 'wp_cron_fix_scheduled_missed_timeout',  time(), 200 );
+		wp_cron_fix_scheduled_post_do_log("Old process still running");
+		
+		if(!$from_front)
+			return true;
+	}
+
+	set_transient( 'wp_cron_fix_scheduled_missed_timeout',  time(), 100 );
 
 	global $wpdb;
 
@@ -57,16 +53,26 @@ function wp_cron_fix_scheduled_post_do()
 
 	$scheduledIDs = $wpdb->get_col( $sql );
 
-	if ( ! count( $scheduledIDs ) )
-		return;
+	if ( ! count( $scheduledIDs ) ) {
+		
+		wp_cron_fix_scheduled_post_do_log("no missed post");
 
-	foreach ( $scheduledIDs as $scheduledID )
-	{
+		return true;
+	}
+	
+	wp_cron_fix_scheduled_post_do_log("Missed post : ".implode(",", $scheduledIDs ));
+
+	foreach ( $scheduledIDs as $scheduledID ) {
 		if ( ! $scheduledID )
 			continue;
 
 		wp_publish_post( $scheduledID );
 	}
+
+	if($from_front)
+		return $scheduledIDs;
+	else
+		return true;
 }
 
 // Add custom cron interval
@@ -75,10 +81,60 @@ add_filter( 'cron_schedules', 'wp_cfsp_add_custom_cron_intervals', 10, 1 );
 function wp_cfsp_add_custom_cron_intervals( $schedules ) {
 	// $schedules stores all recurrence schedules within WordPress
 	$schedules['wp_fsp_add_custom_cron_intervals_five_minutes'] = array(
-		'interval'	=> 300,	// Number of seconds, 600 in 10 minutes
+		'interval'	=> 30,	// Number of seconds
 		'display'	=> 'Once Every 5 Minutes'
 	);
 
 	// Return our newly added schedule to be merged into the others
 	return (array)$schedules; 
+}
+
+function wp_cron_fix_scheduled_post_do_log( $message = false ) {
+
+	if(!defined('CFSP_LOG'))
+		define('CFSP_LOG',false);
+	
+	if( !CFSP_LOG )
+		return;
+
+	global $wp_filesystem;
+
+	$url = wp_nonce_url('plugins.php');
+
+	$dir = plugin_dir_path( __FILE__ );
+
+	global $wp_filesystem;
+
+	// Initialize the WP filesystem, no more using 'file-put-contents' function
+	if (empty($wp_filesystem)) {
+	    require_once (ABSPATH . '/wp-admin/includes/file.php');
+	    WP_Filesystem();
+	}
+
+	// if(!$wp_filesystem->put_contents( $path, $css, 0644) ) {
+	    // return __('Failed to create css file');
+	// }
+
+	// if (false === ($creds = request_filesystem_credentials($url, '', false, $dir, null) ) ) {
+	//     echo "Could not create filesystem credentials";
+	//     return;
+	// }
+
+	// if ( ! WP_Filesystem($creds) ) {
+	//     request_filesystem_credentials($url, '', true, $dir, null);
+	//     echo "Filesystem credentials were not available";
+	//     return;
+	// }
+
+	//@todo if size > 1mb then create new file
+	$text = $wp_filesystem->get_contents($dir.'log.txt');
+
+	$message = "[".current_time( 'mysql' )."] ".$message;
+
+	$text = $text.$message."\n";
+
+	$wp_filesystem->put_contents(
+	  $dir.'log.txt',
+	  $text
+	 );
 }
